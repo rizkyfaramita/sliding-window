9f84766a6b38fdea74b3cbe485ae15f6b234a8a5
Jauhar Arifin <jauhararifin10@gmail.com>
2017-10-15 17:47:49 +0700

    restructure code

--- server.c
+++ server.c
@@ -3,8 +3,6 @@
 #include <stdlib.h> 
 #include <arpa/inet.h>
 #include <sys/socket.h>
- 
-#define BUFLEN 512
 
 typedef struct {
     char soh; // 0
@@ -17,7 +15,7 @@ typedef struct {
 
 int to_raw(segment seg, char* buffer) {
     buffer[0] = seg.soh;
-    char* x = &seg.seq;
+    char* x = (char*) &seg.seq;
     buffer[1] = *x;
     buffer[2] = *(x+1);
     buffer[3] = *(x+2);
@@ -27,6 +25,20 @@ int to_raw(segment seg, char* buffer) {
     buffer[7] = seg.etx;
     buffer[8] = seg.checksum;
     return 9;
+}
+
+void to_segment(char* raw, segment* seg) {
+    seg->soh = *raw;
+    seg->seq = ((int) *(raw+1))<<24 | ((int) *(raw+2))<<16 | ((int) *(raw+3))<<16 | ((int) *(raw+4));
+    seg->stx = *(raw + 5);
+    seg->data = *(raw + 6);
+    seg->etx = *(raw + 7);
+    seg->checksum = *(raw + 8);
+}
+
+void hex(char* s, int n) {
+    while (n--)
+        printf("%02x ", (*(s++) & 0xff));
 }
  
 int to_int(char* str) {
@@ -40,50 +52,83 @@ void die(char *s) {
     perror(s);
     exit(1);
 }
- 
-int main(int argc, char** argv) {
-    if (argc < 5)
-        die("Usage : ./recvfile <filename> <windowsize> <buffersize> <port>");
-    printf("a\n"); fflush(stdout);
 
-    int sockfd, port = to_int(argv[4]), recv_len;
-    struct sockaddr_in si_me, si_other;
-    char buf[BUFLEN];
-    int slen = sizeof(si_other);
-
+void init_socket(int port, int *sockfd) {
     //create a UDP socket
-    if ((sockfd=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    if ((*sockfd=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
         die("Cannot creating socket instance");
-    printf("Socket descriptor created\n"); fflush(stdout);
+    printf("Socket descriptor created\n");
+    fflush(stdout);
 
     // zero out the structure
+    struct sockaddr_in si_me;
     memset(&si_me, 0, sizeof(si_me));
-
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(port); 
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
     //bind socket to port
-    if(bind(sockfd , (struct sockaddr*) &si_me, sizeof(si_me)) == -1)
+    printf("Binding socket to port %d\n", port);
+    fflush(stdout);
+    if(bind(*sockfd , (struct sockaddr*) &si_me, sizeof(si_me)) == -1)
         die("Cannot bind socket to specific port");
-    printf("Socket bind\n"); fflush(stdout);
+    printf("Socket bind on port %d\n", port);
+    fflush(stdout);
+}
+
+char checksum(char x) {
+    return x;
+}
+ 
+int main(int argc, char** argv) {
+    if (argc < 5)
+        die("Usage : ./recvfile <filename> <windowsize> <buffersize> <port>");
+
+    int sockfd;
+    int port = to_int(argv[4]);
+
+    init_socket(port, &sockfd);
+    printf("Finish initializing socket\n"); fflush(stdout);
 
     //keep listening for data
     while(1) {
-        printf("Waiting for data...");
-        fflush(stdout);
+        int len;
+        char buff[256];
+
+        len = recv(sockfd, buff, 9, 0);
+        if (len < 0)
+            die("Some error occured when reading buffer");
+        if (*buff == '\01' && *(buff+5) == '\02' && *(buff+7) == '\03') {
+            segment seg; to_segment(buff, &seg);
+            printf("Segment caught\n");
+            printf("  SOH      : 0x%02x\n", seg.soh);
+            printf("  segnum   : 0x%02x (%d in decimal)\n", seg.seq, seg.seq);
+            printf("  STX      : 0x%02x\n", seg.stx);
+            printf("  data     : 0x%02x\n", seg.data & 0xff);
+            printf("  ETX      : 0x%02x\n", seg.etx);
+            printf("  checksum : 0x%02x\n", seg.checksum & 0xff);
+
+            if (checksum(seg.data) != seg.checksum) {
+                printf("Checksum error: calculated %02x, expected %02x\n\r", checksum(seg.data) & 0xff, seg.checksum & 0xff);
+            } else {
+                
+            }
+
+            fflush(stdout);
+        }
 
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(sockfd, buf, BUFLEN, 0, (struct sockaddr*) &si_other, &slen)) == -1)
-            die("Some error occured when recvfrom() is called");
+        // if ((recv_len = recvfrom(sockfd, buf, 1, 0, (struct sockaddr*) &si_other, &slen)) == -1)
+        //     die("Some error occured when recvfrom() is called");
+        // printf("Starting data : "); hex(buf, 1); printf("\n");
+        // if (strcmp(buf, "\x01") == 0) {
+        //     if ((recv_len = recvfrom(sockfd, buf + 1, 8, 0, (struct sockaddr*) &si_other, &slen)) == -1)
+        //         die("Some error occured when recvfrom() is called");
+        //     printf("Data: ");hex(buf, 9);printf("\n");
+        //     segment msg; to_segment(buf, &msg);
 
-        //print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n" , buf);
-
-        //now reply the client with the same data
-        if (sendto(sockfd, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-            die("Some error occured when sendto() is called");
+        //     // printf("%d %d %d %d %d %d\n", msg.soh, msg.seq, msg.stx, msg.data, msg.etx, msg.checksum);
+        // }
     }
 
     close(sockfd);
